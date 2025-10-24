6dde046dc3a39a057af834950ac3ae4966ccadae
Мурад Салманов <Salmanov@blue2.org>
2025-10-24 13:08:19 +0300

    Обновлен файл B9.c

--- B9.c
+++ B9.c
@@ -1,47 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
-{
-    long long n;
-    // printf("Введите простое число: ");
-    if (scanf("%lld", &n) != 1)
-    {
-        // printf("Некорректный ввод\n");
-        return 1;
-    }
-
-    if (n < 0)
-    {
-        printf("NO\n");
-        return 0;
-    }
-
-    if (n >= 0 && n <= 9)
-    {
-        printf("YES\n");
-        return 0;
-    }
-
-    long long prev_digit = 10; // больше любой цифры
-    long long temp = n;
-    int is_ascending = 1;
-
-    while (temp > 0)
-    {
-        long long digit = temp % 10;
-        if (digit >= prev_digit)
-        {
-            is_ascending = 0;
-            break;
+int main(void) {
+    char s[10005];
+    scanf("%10004s", s);
+    int start = (s[0] == '-' || s[0] == '+') ? 1 : 0;
+    for (int i = start; s[i]; i++) {
+        if ((s[i] - '0') % 2 != 0) {
+            printf("NO\n");
+            return 0;
         }
-        prev_digit = digit;
-        temp /= 10;
     }
-
-    if (is_ascending)
-        printf("YES\n");
-    else
-        printf("NO\n");
-
+    printf("YES\n");
     return 0;
 }
